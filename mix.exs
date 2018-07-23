ba835359c5ca81a2445a16c93a88005e56e73e06
Faraz Haider <frz.hdr.619@gmail.com>
2018-07-23 22:50:05 +0530

    Added exprotobuf library for Protocol Buffers support

--- mix.exs
+++ mix.exs
@@ -14,13 +14,14 @@ defmodule Bargad.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:exprotobuf, :logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:exprotobuf, "~> 1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
