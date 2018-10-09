895b5123d659a5240b00030942c7591c940d6263
Faraz Haider <frz.hdr.619@gmail.com>
2018-10-09 22:22:13 +0530

    Updated mix version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bargad.MixProject do
   def project do
     [
       app: :bargad,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
